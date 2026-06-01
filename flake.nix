d408a1cbc363a08f17479ebd58d94844ec813ce2
Pravdyvy <awxrvtb@gmail.com>
2026-06-01 14:42:25 +0300

    feat: dependency update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    logos-execution-zone.url = "github:logos-blockchain/lssa?rev=cf3639d8252040d13b3d4e933feb19b42c76e14a"; # v0.1.2
+    logos-execution-zone.url = "github:logos-blockchain/lssa?rev=18e1bea512ab0c74e7ee701871628c93168b80ce"; # branch with the latest functionality
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
