a29c878dd3a39f3125f4a465094ac5d20962d5c7
Sergio Chouhy <sergio.chouhy@gmail.com>
2026-09-11 12:30:55 -0300

    bump lez to v0.2.4

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    logos-execution-zone.url = "github:logos-blockchain/logos-execution-zone?ref=v0.2.2";
+    logos-execution-zone.url = "github:logos-blockchain/logos-execution-zone?ref=v0.2.4";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
